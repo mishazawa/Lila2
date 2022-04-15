502f456519a56a6dbcf117c0f11e228496c97ac7
mszw <macintoshkrug@gmail.com>
2022-04-15 22:21:30 +0300

    fix houdini engine

--- Assets/Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
+++ Assets/Plugins/HoudiniEngineUnity/Scripts/HEU_HoudiniVersion.cs
@@ -41,10 +41,10 @@ namespace HoudiniEngineUnity
         {
                 public const int HOUDINI_MAJOR			= 19;
                 public const int HOUDINI_MINOR			= 0;
-                public const int HOUDINI_BUILD			= 561;
+                public const int HOUDINI_BUILD			= 589;
                 public const int HOUDINI_PATCH			= 0;
 
-                public const string HOUDINI_VERSION_STRING = "19.0.561";
+                public const string HOUDINI_VERSION_STRING = "19.0.589";
 
                 public const int HOUDINI_ENGINE_MAJOR	= 4;
                 public const int HOUDINI_ENGINE_MINOR	= 2;
